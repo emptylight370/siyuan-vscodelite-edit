d34dd8fa4a61a84f8085b890face603af3de8e9d
Emptylight <1378990254@qq.com>
2025-05-25 13:06:29 +0800

    style: 修改Prettier设置

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import typescript from '@rollup/plugin-typescript';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import typescript from "@rollup/plugin-typescript";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
-    input: resolve(__dirname, 'src/theme.ts'),
+    input: resolve(__dirname, "src/theme.ts"),
     output: {
-        file: resolve(__dirname, './theme.js'),
-        format: 'iife',          // 修改为 iife 格式
+        file: resolve(__dirname, "./theme.js"),
+        format: "iife", // 修改为 iife 格式
     },
     plugins: [
         typescript({
-            tsconfig: resolve(__dirname, './tsconfig.json'),
-        })
-    ]
+            tsconfig: resolve(__dirname, "./tsconfig.json"),
+        }),
+    ],
 };
