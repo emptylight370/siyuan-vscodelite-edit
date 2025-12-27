2e6e364772aa1fcea449790591dfeccd0610b937
Emptylight <1378990254@qq.com>
2025-12-28 01:00:01 +0800

    feat: 优化获取本地化文本函数

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -149,21 +149,21 @@ export async function _reloadInterface() {
  * @since 2.6.3
  * @version 2.6.3
  */
-export function getMsg(msg: keyof vscMessage): string {
+export function getMsg(msg: keyof vscMessage) {
     // 当前的本地化文本对象
-    const msgObj = globalThis.vscMessage[msg];
+    const msgObj = globalThis.vscMessage[msg] as Record<string, string>;
     // 首先尝试获取当前语言版本
     if (msgObj[globalThis.vscLang]) {
-        return msgObj[globalThis.vscLang] as string;
+        return msgObj[globalThis.vscLang];
     }
     // 如果当前语言不存在，尝试回退到en_US
     if (msgObj["en_US"]) {
-        return msgObj["en_US"] as string;
+        return msgObj["en_US"];
     }
     // 如果en_US也不存在，则返回第一个可用的值
     const langs = Object.keys(msgObj);
     if (langs.length > 0) {
-        return msgObj[langs[0]] as string;
+        return msgObj[langs[0]];
     }
     // 如果没有可用的文本，返回报错信息
     return `vscMessage.${msg}.${globalThis.vscLang}`;
