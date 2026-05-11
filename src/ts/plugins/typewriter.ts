0d2368ce225ec14503c624ef0d3d5deb03e5707f
Emptylight <1378990254@qq.com>
2026-05-11 11:41:41 +0800

    fix: 修复打字机模式画廊视图操作按钮跳转问题

--- src/ts/plugins/typewriter.ts
+++ src/ts/plugins/typewriter.ts
@@ -45,6 +45,7 @@ function scrollToCenter(sourceElement?: HTMLElement): void {
     // 如果点击的元素是个按钮，则跳过
     else if (currentTargetElement.closest(".block__icon")) return;
     else if (currentTargetElement.closest(".protyle-icons")) return;
+    else if (currentTargetElement.closest(".av__gallery-actions")) return;
 
     // 从光标位置的容器节点向上查找最近的 [data-node-id] 元素
     let targetElement: HTMLElement | null = currentTargetElement.closest("[data-node-id]") as HTMLElement;
